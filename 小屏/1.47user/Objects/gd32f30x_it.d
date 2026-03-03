f1aaf22d7d337c280346d68e4444853577f48bf2
baixiaodong <baixiaodong@lingchuang-dl.com>
2026-03-03 17:05:18 +0800

    mcu panel

---
+++ 小屏/1.47user/Objects/gd32f30x_it.d
@@ -0,0 +1,11 @@
+./objects/gd32f30x_it.o: gd32f30x_it.c gd32f30x_it.h \
+  C:\Users\Administrator\AppData\Local\Arm\Packs\GigaDevice\GD32F30x_DFP\2.2.3\Device\Include\gd32f30x.h \
+  C:\Users\Administrator\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\core_cm4.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Users\Administrator\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\Administrator\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\Administrator\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\Users\Administrator\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\mpu_armv7.h \
+  C:\Users\Administrator\AppData\Local\Arm\Packs\GigaDevice\GD32F30x_DFP\2.2.3\Device\Include\system_gd32f30x.h \
+  C:\Users\Administrator\AppData\Local\Arm\Packs\GigaDevice\GD32F30x_DFP\2.2.3\Device\Template\gd32f30x_libopt.h \
+  RTE\_Target_1\RTE_Components.h systick.h
---
+++ 小屏/1.47user/RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,23 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'user-test' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "gd32f30x.h"
+
+/* GigaDevice::Device:GD32F30x_StdPeripherals:RCU:2.2.0 */
+#define RTE_DEVICE_STDPERIPHERALS_RCU
+
+
+#endif /* RTE_COMPONENTS_H */
